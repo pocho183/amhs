01a955de92591395c6417ce4a7de486dac4ed15b
Manuel <manuel.corini@gmail.com>
2026-03-07 19:59:22 +0100

    Implement ICAO-compliant directional ACSE association semantics

--- src/test/java/it/amhs/service/AcseModelsTest.java
+++ src/test/java/it/amhs/service/AcseModelsTest.java
@@ -24,6 +24,31 @@ class AcseModelsTest {
     }
 
     @Test
+    void shouldHandleInboundAssociationHandshakeBeforeEstablished() {
+        AcseModels.AssociationStateMachine sm = new AcseModels.AssociationStateMachine();
+
+        sm.onInbound(new AcseModels.AARQApdu("2.6.0.1.6.1", Optional.of("CALLING"), Optional.of("CALLED")));
+        assertEquals(AcseModels.AssociationState.AWAITING_AARE_RESPONSE, sm.state());
+
+        sm.onOutbound(new AcseModels.AAREApdu(true, Optional.empty()));
+        assertEquals(AcseModels.AssociationState.ESTABLISHED, sm.state());
+    }
+
+    @Test
+    void shouldHandleInboundReleaseHandshake() {
+        AcseModels.AssociationStateMachine sm = new AcseModels.AssociationStateMachine();
+
+        sm.onOutbound(new AcseModels.AARQApdu("2.6.0.1.6.1", Optional.of("CALLING"), Optional.of("CALLED")));
+        sm.onInbound(new AcseModels.AAREApdu(true, Optional.empty()));
+
+        sm.onInbound(new AcseModels.RLRQApdu(Optional.of("normal")));
+        assertEquals(AcseModels.AssociationState.AWAITING_RLRE_RESPONSE, sm.state());
+
+        sm.onOutbound(new AcseModels.RLREApdu(true));
+        assertEquals(AcseModels.AssociationState.CLOSED, sm.state());
+    }
+
+    @Test
     void shouldRejectInvalidTransition() {
         AcseModels.AssociationStateMachine sm = new AcseModels.AssociationStateMachine();
         assertThrows(IllegalStateException.class, () -> sm.onOutbound(new AcseModels.RLRQApdu(Optional.empty())));
